66607be6c3f62f4b59a7bc881b8f35b3da151a24
NJSee <48836977+njsee@users.noreply.github.com>
2020-11-06 17:49:52 -0800

    updated map

--- output.tf
+++ output.tf
@@ -3,8 +3,7 @@ output "seclogs_map" {
     description = "Ouputs a map with storage account id (activity_sa), eventhub name (activity_eh_name) and id (activity_eh_id) - if enabled"
     depends_on = [ azurerm_storage_account.log ]
 
-    value =
-        map(
+    value = map(
             "activity_sa", azurerm_storage_account.log.id,
             "activity_eh_name", var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].name : null,
             "activity_eh_id", var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].id : null, 
